641c470a5b8b02f820dab37c86c2ea2343659349
WT <go.to.another5@gmail.com>
2026-04-24 14:13:15 +0300

    Create REPLACE_TRANSLATIONS.gd

---
+++ HEVLIB_MENU/REPLACE_TRANSLATIONS.gd
@@ -0,0 +1,182 @@
+extends Node
+
+# This translation file is generated automatically
+# Do not modify anything directly, as this can break things for those working on them
+# Please use Translation Tracker to modify these yourself, and contact the mod author to implement them
+# https://github.com/rwqfsfasxc100/TranslationTracker/releases/latest
+
+const TRANSLATIONS = {
+	"master_locale": "en",
+	"en": {
+		"MOARRADARS_MOD_DESCRIPTION": {
+			"string": "Mod that introduces new RADARs",
+			"version_hash": 1471896669
+		},
+		"SYSTEM_RADAR_DOPPLER": {
+			"string": "DSR-100 Doppler Sweeping RADAR",
+			"version_hash": 2724529454
+		},
+		"SYSTEM_RADAR_DOPPLER_DESC": {
+			"string": "A standard all-purpose radar that balances range, resolution, and sweep frequency, this model offers reliable Doppler-shift detection. While it lacks the advanced features of specialized radar systems, it provides a dependable solution for general navigation and threat detection.",
+			"version_hash": 3500399835
+		},
+		"SYSTEM_RADAR_DOPPLER_SPECS": {
+			"string": "Manufacturer: Conlido RVM \\nScanning beam: low-frequency radio \\nSweep period: 500 ms \\nSweep coverage: 360 deg \\nMeasurements: distance, relative velocity",
+			"version_hash": 2967332175
+		},
+		"SYSTEM_RADAR_DOPPLER_MANUAL": {
+			"string": "Scans the surroundings of your ship. \\nPierces ice and rock formations, providing vague ice readouts. \\nPresents relative velocity of detected objects with color shifts. \\nDetects only ships and metallic ore nuggets.",
+			"version_hash": 3052720623
+		},
+		"SYSTEM_RADAR_CONE": {
+			"string": "BF-220 Beamformed RADAR",
+			"version_hash": 4111516938
+		},
+		"SYSTEM_RADAR_CONE_DESC": {
+			"string": "This cutting-edge beamformed radar system concentrates its low-frequency radio waves into a narrow beam, enhancing detection precision and speed for forward-facing scans. Primarily used in exploration and navigation, it excels at detecting hidden structures or potential threats ahead. However, its limited field of view makes it unsuitable for all-around monitoring.",
+			"version_hash": 2429145598
+		},
+		"SYSTEM_RADAR_CONE_SPECS": {
+			"string": "Manufacturer: Minding LF \\nScanning beam: low-frequency radio \\nSweep period: 500 ms \\nSweep coverage: 30 deg \\nMeasurements: distance",
+			"version_hash": 1230996637
+		},
+		"SYSTEM_RADAR_CONE_MANUAL": {
+			"string": "Scans the surroundings of your ship. \\nPierces ice and rock formations, providing vague ice readouts. \\nDetects objects in a narrow cone. \\nDetects only ships and metallic ore nuggets.",
+			"version_hash": 1768555535
+		},
+		"SYSTEM_RADAR_OPA": {
+			"string": "PR-500 Phased RADAR Array",
+			"version_hash": 2202237415
+		},
+		"SYSTEM_RADAR_OPA_DESC": {
+			"string": "An advanced phased array radar system that can electronically steer its beam in any direction, eliminating the need for moving parts. The PR-500 offers near-instantaneous area scanning and excellent accuracy in tracking moving objects, though it struggles with detecting small, fast-moving targets at extreme ranges.",
+			"version_hash": 1672866598
+		},
+		"SYSTEM_RADAR_OPA_SPECS": {
+			"string": "Manufacturer: Coherent Light \\nScanning beam: medium-frequency radio \\nSweep period: continuous \\nSweep coverage: 360 deg \\nMeasurements: distance",
+			"version_hash": 1964559356
+		},
+		"SYSTEM_RADAR_OPA_MANUAL": {
+			"string": "Scans the surroundings of your ship. \\nPierces ice and rock formations, providing vague ice readouts. \\nDetects only ships and metallic ore nuggets. \\nHigh resolution. \\nVery high scanning speed. \\nAffected by ghosting artifacts.",
+			"version_hash": 1810258720
+		},
+		"SYSTEM_RADAR_OPA_DOPPLER": {
+			"string": "PR-500D Doppler Phased RADAR",
+			"version_hash": 2111761282
+		},
+		"SYSTEM_RADAR_OPA_DOPPLER_DESC": {
+			"string": "Building upon the PR-500, this upgraded phased array radar incorporates Doppler-shift detection to provide superior tracking of object velocities. This enhancement significantly improves its threat detection capabilities, though the system remains sensitive to interference and minor ghosting artifacts.",
+			"version_hash": 876217843
+		},
+		"SYSTEM_RADAR_OPA_DOPPLER_SPECS": {
+			"string": "Manufacturer: Coherent Light \\nScanning beam: medium-frequency radio \\nSweep period: continuous \\nSweep coverage: 360 deg \\nMeasurements: distance, relative velocity",
+			"version_hash": 1587257363
+		},
+		"SYSTEM_RADAR_OPA_DOPPLER_MANUAL": {
+			"string": "Scans the surroundings of your ship. \\nPierces ice and rock formations, providing vague ice readouts. \\nPresents relative velocity of detected objects with color shifts. \\nDetects only ships and metallic ore nuggets. \\nHigh resolution. \\nVery high scanning speed. \\nAffected by ghosting artifacts.",
+			"version_hash": 3003385028
+		},
+		"SYSTEM_RADAR_HYBRID": {
+			"string": "RLS-900 Hybrid RADAR-LIDAR System",
+			"version_hash": 1056799494
+		},
+		"SYSTEM_RADAR_HYBRID_DESC": {
+			"string": "A cutting-edge hybrid system that combines high-resolution LIDAR with medium-frequency radar for exceptional performance in both short and long-range detection. The RLS-900 uses LIDAR for precise close-range scans and radar for broader area coverage, making it ideal for scientific missions and exploration. The system provides detailed measurements of distance and object shapes but lacks velocity tracking, limiting its ability to assess moving targets in real-time.",
+			"version_hash": 2612749831
+		},
+		"SYSTEM_RADAR_HYBRID_SPECS": {
+			"string": "Manufacturer: Runasimi \\n Scanning beam: infrared light, medium-frequency radio \\nSweep period: 2000 ms \\nSweep coverage: 360 deg \\nMeasurements: distance",
+			"version_hash": 3064196462
+		},
+		"SYSTEM_RADAR_HYBRID_MANUAL": {
+			"string": "Scans the surroundings of your ship. \\nDetects all visible objects. \\Pierces ice and rock formations. \\nHigh resolution. \\nSlow scanning speed.",
+			"version_hash": 3413432801
+		}
+	},
+	"uk_UA": {
+		"MOARRADARS_MOD_DESCRIPTION": {
+			"string": "Мод, що додає нові РАДАРИ",
+			"version_hash": 1471896669
+		},
+		"SYSTEM_RADAR_DOPPLER": {
+			"string": "Доплерівський скануючий РАДАР DSR-100",
+			"version_hash": 2724529454
+		},
+		"SYSTEM_RADAR_DOPPLER_DESC": {
+			"string": "Стандартний багатоцільовий радар, що балансує між дальністю, роздільною здатністю та частотою сканування, ця модель пропонує надійне виявлення доплерівського зсуву. Хоча йому бракує передових функцій спеціалізованих радарних систем, він забезпечує надійне рішення для загальної навігації та виявлення загроз.",
+			"version_hash": 3500399835
+		},
+		"SYSTEM_RADAR_DOPPLER_SPECS": {
+			"string": "Виробник: Conlido RVM \\nСкануючий промінь: низькочастотні радіохвилі \\nТривалість циклу: 500 мс \\nКут охоплення: 360 град \\nВимірювання: дистанція, відносна швидкість",
+			"version_hash": 2967332175
+		},
+		"SYSTEM_RADAR_DOPPLER_MANUAL": {
+			"string": "Сканує оточення корабля. \\nПроникає крізь крижані та кам'яні утворення, відображає їх з низькою точністю. \\nВідображає кольором відносну швидкість виявлених об'єктів. \\nВиявляє тільки кораблі та металеві руди.",
+			"version_hash": 3052720623
+		},
+		"SYSTEM_RADAR_CONE": {
+			"string": "Спрямований РАДАР BF-220",
+			"version_hash": 4111516938
+		},
+		"SYSTEM_RADAR_CONE_DESC": {
+			"string": "Ця передова спрямована радарна система концентрує низькочастотні радіохвилі у вузький промінь, підвищуючи точність виявлення та швидкість для сканування попереду. В основному використовується для розвідки та навігації, відмінно виявляє приховані структури або потенційні загрози попереду. Однак обмежене поле зору робить його непридатним для кругового спостереження.",
+			"version_hash": 2429145598
+		},
+		"SYSTEM_RADAR_CONE_SPECS": {
+			"string": "Виробник: Minding LF \\nСкануючий промінь: низькочастотні радіохвилі \\nТривалість циклу: 500 мс \\nКут охоплення: 30 град \\nВимірювання: дистанція",
+			"version_hash": 1230996637
+		},
+		"SYSTEM_RADAR_CONE_MANUAL": {
+			"string": "Сканує оточення корабля. \\nПроникає крізь крижані та кам'яні утворення, відображає їх з низькою точністю. \\nВиявляє об'єкти у вузькому конусі. \\nВиявляє тільки кораблі та металеві руди.",
+			"version_hash": 1768555535
+		},
+		"SYSTEM_RADAR_OPA": {
+			"string": "Фазований РАДАР PR-500",
+			"version_hash": 2202237415
+		},
+		"SYSTEM_RADAR_OPA_DESC": {
+			"string": "Передова система фазованого радара, яка може електронно керувати променем у будь-якому напрямку, усуваючи необхідність у рухомих частинах. PR-500 пропонує майже миттєве сканування території та відмінну точність у відстеженні рухомих об'єктів, хоча має проблеми з виявленням малих швидкорухомих цілей на екстремальних відстанях.",
+			"version_hash": 1672866598
+		},
+		"SYSTEM_RADAR_OPA_SPECS": {
+			"string": "Виробник: Coherent Light \\nСкануючий промінь: середньочастотні радіохвилі \\nТривалість циклу: безперервний \\nКут охоплення: 360 град \\nВимірювання: дистанція",
+			"version_hash": 1964559356
+		},
+		"SYSTEM_RADAR_OPA_MANUAL": {
+			"string": "Сканує оточення корабля. \\nПроникає крізь крижані та кам'яні утворення, відображає їх з низькою точністю. \\nВиявляє тільки кораблі та металеві руди. \\nВисока роздільна здатність. \\nДуже висока швидкість сканування. \\nСхильний до ефекту залишкового зображення.",
+			"version_hash": 1810258720
+		},
+		"SYSTEM_RADAR_OPA_DOPPLER": {
+			"string": "Доплерівський фазований РАДАР PR-500D",
+			"version_hash": 2111761282
+		},
+		"SYSTEM_RADAR_OPA_DOPPLER_DESC": {
+			"string": "Спираючись на PR-500, цей покращений фазований радар включає виявлення доплерівського зсуву для кращого відстеження швидкостей об'єктів. Це вдосконалення значно покращує можливості виявлення загроз, хоча система залишається чутливою до перешкод та незначних ефектів залишкового зображення.",
+			"version_hash": 876217843
+		},
+		"SYSTEM_RADAR_OPA_DOPPLER_SPECS": {
+			"string": "Виробник: Coherent Light \\nСкануючий промінь: середньочастотні радіохвилі \\nТривалість циклу: безперервний \\nКут охоплення: 360 град \\nВимірювання: дистанція, відносна швидкість",
+			"version_hash": 1587257363
+		},
+		"SYSTEM_RADAR_OPA_DOPPLER_MANUAL": {
+			"string": "Сканує оточення корабля. \\nПроникає крізь крижані та кам'яні утворення, відображає їх з низькою точністю. \\nВідображає кольором відносну швидкість виявлених об'єктів. \\nВиявляє тільки кораблі та металеві руди. \\nВисока роздільна здатність. \\nДуже висока швидкість сканування. \\nСхильний до ефекту залишкового зображення.",
+			"version_hash": 3003385028
+		},
+		"SYSTEM_RADAR_HYBRID": {
+			"string": "Гібридна система РАДАР-ЛІДАР RLS-900",
+			"version_hash": 1056799494
+		},
+		"SYSTEM_RADAR_HYBRID_DESC": {
+			"string": "A cutting-edge hybrid system that combines high-resolution LIDAR with medium-frequency radar for exceptional performance in both short and long-range detection. The RLS-900 uses LIDAR for precise close-range scans and radar for broader area coverage, making it ideal for scientific missions and exploration. The system provides detailed measurements of distance and object shapes but lacks velocity tracking, limiting its ability to assess moving targets in real-time.",
+			"version_hash": 2612749831
+		},
+		"SYSTEM_RADAR_HYBRID_SPECS": {
+			"string": "Виробник: Runasimi \\nСкануючий промінь: інфрачервоне світло, середньочастотні радіохвилі \\nТривалість циклу: 2000 мс \\nКут охоплення: 360 град \\nВимірювання: дистанція",
+			"version_hash": 3064196462
+		},
+		"SYSTEM_RADAR_HYBRID_MANUAL": {
+			"string": "Сканує оточення корабля. \\nВиявляє всі видимі об'єкти. \\nПроникає крізь крижані та кам'яні утворення. \\nВисока роздільна здатність. \\nПовільна швидкість сканування.",
+			"version_hash": 3413432801
+		}
+	}
+}
