6f14c14ea08388a0d485ed3a27cf232659de1481
WT <go.to.another5@gmail.com>
2026-07-22 19:52:36 +0300

    v2.1.0
    * Convert to modlet
    * Add LICENSE
    * Clean unused i18n

---
+++ HEVLIB_DRIVERS/LOAD_RESOURCES.gd
@@ -0,0 +1,8 @@
+extends Node
+
+const LOAD_RESOURCES = {
+	"hud/LIDAR.gd": {"load_type": "script"},
+	"hud/components/LIDAR.tscn": {"load_type": "scene"},
+	"enceladus/Upgrades.tscn": {"load_type": "scene"},
+	"enceladus/Tuning.tscn": {"load_type": "scene"}
+}
